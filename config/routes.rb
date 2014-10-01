640527346162389bffb21cd7aba4eecc19d14366
yunixon <yunixon@gmail.com>
2014-10-01 16:11:31 +0400

    fix API and add projects migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141001121017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: true do |t|
+    t.string "name"
+    t.text   "description"
+    t.string "status"
+    t.string "link"
+    t.string "photo"
+  end
 
 end
